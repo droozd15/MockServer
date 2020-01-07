8be6d39a49ee14369ff5aa82032d3569ca0a9b83
AZanovskaya <azanovskaya@softrust.ru>
2020-01-07 18:07:56 +0300

    проверяем сервер

---
+++ Test4v2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test4v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
